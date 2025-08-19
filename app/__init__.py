1756e1288782ae6b7c713f26f4367768a85c07c6
waimea-nawaite <nawaite@waimea.school.nz>
2025-08-19 15:10:07 +1200

    did good stuff

--- app/__init__.py
+++ app/__init__.py
@@ -26,11 +26,19 @@ init_datetime(app)  # Handle UTC dates in timestamps
 
 
 #-----------------------------------------------------------
-# Home page route
+# Home page route shows garments
 #-----------------------------------------------------------
 @app.get("/")
-def index():
-    return render_template("pages/home.jinja")
+def show_all_garments():
+    with connect_db() as client:
+        # Get all the things from the DB
+        sql = "SELECT priority, name, id FROM garments ORDER BY priority ASC"
+        params = []
+        result = client.execute(sql, params)
+        garments = result.rows
+
+        # And show them on the page
+        return render_template("pages/garments.jinja", garments=garments)
 
 
 #-----------------------------------------------------------
@@ -40,39 +48,22 @@ def index():
 def about():
     return render_template("pages/about.jinja")
 
-
-#-----------------------------------------------------------
-# Things page route - Show all the things, and new thing form
-#-----------------------------------------------------------
-@app.get("/things/")
-def show_all_things():
-    with connect_db() as client:
-        # Get all the things from the DB
-        sql = "SELECT id, name FROM things ORDER BY name ASC"
-        params = []
-        result = client.execute(sql, params)
-        things = result.rows
-
-        # And show them on the page
-        return render_template("pages/things.jinja", things=things)
-
-
 #-----------------------------------------------------------
 # Thing page route - Show details of a single thing
 #-----------------------------------------------------------
-@app.get("/thing/<int:id>")
-def show_one_thing(id):
+@app.get("/garment/<int:id>")
+def show_one_garment(id):
     with connect_db() as client:
         # Get the thing details from the DB
-        sql = "SELECT id, name, price FROM things WHERE id=?"
+        sql = "SELECT id, name, price FROM garments WHERE id=?"
         params = [id]
         result = client.execute(sql, params)
 
         # Did we get a result?
         if result.rows:
             # yes, so show it on the page
-            thing = result.rows[0]
-            return render_template("pages/thing.jinja", thing=thing)
+            garment = result.rows[0]
+            return render_template("pages/garment.jinja", garment=garment)
 
         else:
             # No, so show error
@@ -83,38 +74,38 @@ def show_one_thing(id):
 # Route for adding a thing, using data posted from a form
 #-----------------------------------------------------------
 @app.post("/add")
-def add_a_thing():
+def add_a_garment():
     # Get the data from the form
     name  = request.form.get("name")
-    price = request.form.get("price")
+    priority = request.form.get("priority")
 
     # Sanitise the text inputs
     name = html.escape(name)
 
     with connect_db() as client:
         # Add the thing to the DB
-        sql = "INSERT INTO things (name, price) VALUES (?, ?)"
-        params = [name, price]
+        sql = "INSERT INTO garments (name, priority) VALUES (?, ?)"
+        params = [name, priority]
         client.execute(sql, params)
 
         # Go back to the home page
-        flash(f"Thing '{name}' added", "success")
-        return redirect("/things")
+        flash(f"Garment '{name}' added", "success")
+        return redirect("/")
 
 
 #-----------------------------------------------------------
 # Route for deleting a thing, Id given in the route
 #-----------------------------------------------------------
 @app.get("/delete/<int:id>")
-def delete_a_thing(id):
+def delete_a_garment(id):
     with connect_db() as client:
         # Delete the thing from the DB
-        sql = "DELETE FROM things WHERE id=?"
+        sql = "DELETE FROM garments WHERE id=?"
         params = [id]
         client.execute(sql, params)
 
         # Go back to the home page
-        flash("Thing deleted", "success")
-        return redirect("/things")
+        flash("Garment deleted", "success")
+        return redirect("/")
 
 
